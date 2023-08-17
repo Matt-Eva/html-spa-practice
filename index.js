6f8e693ee1161950b8c622648e9cc8135632467f
Matt Eva <matthew.eva@flatironschool.com>
2023-08-17 10:47:47 -0700

    added offline check

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const main = document.querySelector("main")
+console.log(main)
+
+console.log(navigator.onLine)
+
 window.addEventListener("resize", navigateCurrentUrl)
 
 window.addEventListener("hashchange", navigateCurrentUrl)
@@ -10,3 +15,16 @@ function navigateCurrentUrl(){
 }
 
 console.log("hello")
+
+fetch("http://localhost:3000/data")
+.then(r => r.json())
+.then(console.log)
+.catch(e =>{
+    console.log("It looks like you're offline at the moment")
+    // console.log(e)
+    const h2 = document.createElement("h2")
+    h2.textContent = "It looks like you're offline at the moment. Please connect to wifi to continue."
+    h2.style.position = "fixed"
+    h2.style.top = "80px"
+    main.append(h2)
+})
